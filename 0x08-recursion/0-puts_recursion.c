5c454ce5fcaa5223731f4bfe4a27d759d843139a
Adaobi-Chuks <chuksaginamada@yahoo.com>
2022-04-28 08:50:58 -0700

    all projects

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
+ * _puts_recursion - prints a string followed by a newline
+ * @s: the string to print
  *
- * @s: string
+ * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
+ * _print_rev_recursion - this prints a string in reverse
+ * @s: the string to print in reverse
  *
- * @s: string
+ * Return: nothing
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
 }
