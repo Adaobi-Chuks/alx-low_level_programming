c33cd3b81c93850cafbc8480dc6367b32065bf55
Chuks-Aginam Adaobi <chuksaginamada@yahoo.com>
2022-05-09 20:10:45 +0100

    Corrections

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,8 +8,8 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *s)
-{
+
+int is_digit(char *s) {
 	int i = 0;
 
 	while (s[i])
@@ -27,8 +27,8 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
-int _strlen(char *s)
-{
+
+int _strlen(char *s) {
 	int i = 0;
 
 	while (s[i] != '\0')
@@ -41,8 +41,8 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
-void errors(void)
-{
+
+void errors(void) {
 	printf("Error\n");
 	exit(98);
 }
@@ -54,8 +54,8 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]) {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
